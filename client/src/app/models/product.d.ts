d3e20cf7b4bcffb0afc7b1f83d942f6269725ea9
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-31 05:18:11 +0300

    chore: add global product type

---
+++ client/src/app/models/product.d.ts
@@ -0,0 +1,10 @@
+type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  pictureUrl: string;
+  type: string;
+  category: string;
+  quantityInStock: number;
+};
