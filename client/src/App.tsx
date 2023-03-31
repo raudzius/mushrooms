e4dda5be58be96ddae97029d0ea4d85682da1eb5
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-31 05:18:49 +0300

    feat: display product data fetched from api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+      .then((response) => response.json())
+      .then((products) => {
+        setProducts(products);
+      });
+  }, []);
 
   return (
     <div>
       <h1>Mushroom-Store</h1>
+      <ul>
+        {products.map(({ id, name, price }) => (
+          <li key={id}>
+            {name} - {price}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
