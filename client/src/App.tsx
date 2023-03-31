c215af07f43c5945c007dd07eb6a3efd6bb8df6f
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-03-31 04:38:39 +0300

    feat: create react app

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [products, setProducts] = useState([])
+
+  return (
+    <div>
+      <h1>Mushroom-Store</h1>
+    </div>
+  )
+}
+
+export default App
