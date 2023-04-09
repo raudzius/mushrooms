3dfa11d4924691203d7f8451cd6c7a6d39dc93c8
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-10 00:56:40 +0300

    feat: theme change

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,16 +1,31 @@
-import React, { } from 'react';
-import { Container, CssBaseline } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Container, CssBaseline, createTheme, ThemeProvider,
+} from '@mui/material';
 import Catalog from '../../features/catalog/Catalog';
 import Header from './Header';
 
-const App: React.FC = () => (
-  <>
-    <CssBaseline />
-    <Header />
-    <Container>
-      <Catalog />
-    </Container>
-  </>
-);
+const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const palletteType = darkMode ? 'dark' : 'light';
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+
+  const theme = createTheme({
+    palette: { mode: palletteType },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,9 +1,17 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar, Toolbar, Typography, Switch,
+} from '@mui/material';
 import React from 'react';
 
-const Header: React.FC = () => (
+type HeaderProps = {
+  darkMode: boolean,
+  toggleDarkMode: () => void;
+};
+
+const Header: React.FC<HeaderProps> = ({ darkMode, toggleDarkMode }) => (
   <AppBar position="static" sx={{ mb: 4 }}>
     <Toolbar>
+      <Switch checked={darkMode} onChange={toggleDarkMode} />
       <Typography variant="h6">Mushrooms</Typography>
     </Toolbar>
   </AppBar>
