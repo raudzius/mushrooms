a724eb9def5bdea215a8ce54efd5fa8e8c082002
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-09 22:14:07 +0300

    chore: setup linter for frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
     fetch('http://localhost:5000/api/products')
       .then((response) => response.json())
-      .then((products) => {
-        setProducts(products);
+      .then((productsData) => {
+        setProducts(productsData);
       });
   }, []);
 
@@ -16,13 +16,11 @@ function App() {
       <h1>Mushroom-Store</h1>
       <ul>
         {products.map(({ id, name, price }) => (
-          <li key={id}>
-            {name} - {price}
-          </li>
+          <li key={id}>{`${name}-${price}`}</li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default App;
