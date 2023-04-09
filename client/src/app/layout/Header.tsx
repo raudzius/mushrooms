ba27961fe0e88b68cf437f27b15b8f84d43738a5
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-10 00:27:33 +0300

    feat: add header margin

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -2,7 +2,7 @@ import { AppBar, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 
 const Header: React.FC = () => (
-  <AppBar position="static">
+  <AppBar position="static" sx={{ mb: 4 }}>
     <Toolbar>
       <Typography variant="h6">Mushrooms</Typography>
     </Toolbar>
