31bdfb34753f3f96c2c33e8d5e1da5c5fdf45fbb
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-11 01:24:17 +0300

    feat: add product details page content

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,5 +1,8 @@
-import { Typography } from '@mui/material';
+import {
+  Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography,
+} from '@mui/material';
 import axios from 'axios';
+import Image from 'mui-image';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
@@ -24,7 +27,40 @@ const ProductDetails: React.FC = () => {
   }
 
   return (
-    <Typography variant="h2">{product.name}</Typography>
+    <Grid container spacing={6}>
+      <Grid item xs={6}><Image src={product.pictureUrl} alt={product.name} style={{ width: '100%' }} /></Grid>
+      <Grid item xs={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color="secondary">{`€${(product.price / 100).toFixed(2)}`}</Typography>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>{product.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Description</TableCell>
+                <TableCell>{product.description}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Type</TableCell>
+                <TableCell>{product.type}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Category</TableCell>
+                <TableCell>{product.category}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Quantity in stock</TableCell>
+                <TableCell>{product.quantityInStock}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
   );
 };
 
