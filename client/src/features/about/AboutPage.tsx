beb32059180631c184aed64466e116ba97ef5cb4
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-11 15:28:36 +0300

    test: add temporary error testing buttons

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,8 +1,46 @@
-import { Typography } from '@mui/material';
+import {
+  Button, ButtonGroup, Container, Typography,
+} from '@mui/material';
 import React from 'react';
+import agent from '../../app/api/agent';
 
 const AboutPage: React.FC = () => (
-  <Typography>About Page</Typography>
+  <Container>
+    <Typography gutterBottom variant="h2">Errors for testing purposes</Typography>
+    <ButtonGroup fullWidth>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.get400Error()}
+      >
+        Test 400 Error
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.get401Error()}
+      >
+        Test 401 Error
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.get404Error()}
+      >
+        Test 404 Error
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.get500Error()}
+      >
+        Test 500 Error
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.getValidationError()}
+      >
+        Test Validation Error
+      </Button>
+    </ButtonGroup>
+
+  </Container>
 );
 
 export default AboutPage;
