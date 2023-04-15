ee6eb905fef09b90ce27916b47d43e7aabee9ca9
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-16 02:19:04 +0300

    feat: style basket page with table

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Typography } from '@mui/material';
+import {
+  IconButton,
+  Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography,
+} from '@mui/material';
+import { Delete } from '@mui/icons-material';
 import agent from '../../app/api/agent';
 import LoadingComponent from '../../app/layout/LoadingComponent';
 
@@ -21,7 +25,36 @@ const BasketPage: React.FC = () => {
   if (!basket) return <Typography variant="h3">Your basket is empty</Typography>;
 
   return (
-    <h1>{`Buyer Id = ${basket.buyerId}`}</h1>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Product</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {basket.items.map((basketItem) => (
+            <TableRow
+              key={basketItem.productId}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {basketItem.name}
+              </TableCell>
+              <TableCell align="right">{`€${(basketItem.price / 100).toFixed(2)}`}</TableCell>
+              <TableCell align="right">{basketItem.quantity}</TableCell>
+              <TableCell align="right">
+                {`€${((basketItem.price / 100) * basketItem.quantity).toFixed(2)}`}
+                <IconButton color="error"><Delete /></IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
