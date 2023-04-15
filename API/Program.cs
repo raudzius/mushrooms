17d85516ab050aef316d265cdc5752255a932ac1
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-15 19:17:20 +0300

    feat: add cors allow credentials

--- API/Program.cs
+++ API/Program.cs
@@ -28,7 +28,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors(options =>
 {
-  options.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+  options.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
 });
 
 app.UseAuthorization();
