ab5f18ebcfe390475f6adaaeeed4965c9f9f80ea
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-19 22:14:23 +0300

    feat: add search functionality to api

--- API/Extensions/ProductExtensions.cs
+++ API/Extensions/ProductExtensions.cs
@@ -17,5 +17,13 @@ namespace API.Extensions
 
       return query;
     }
+
+    public static IQueryable<Product> Search(this IQueryable<Product> query, string? searchTerm)
+    {
+      if (string.IsNullOrWhiteSpace(searchTerm)) return query;
+
+      var lowerCaseSearchTerm = searchTerm.Trim().ToLower();
+      return query.Where(product => product.Name!.ToLower().Contains(lowerCaseSearchTerm));
+    }
   }
 }
