bb2a8cb22628d8e6376ba6c564d0e9e3aea113fe
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-20 08:04:36 +0300

    feat: get filter lists from api and store in state

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -2,16 +2,20 @@ import React, { useEffect } from 'react';
 import ProductList from './ProductList';
 import LoadingComponent from '../../app/layout/LoadingComponent';
 import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
-import { fetchProductsAsync, productSelectors } from './catalogSlice';
+import { fetchFiltersAsync, fetchProductsAsync, productSelectors } from './catalogSlice';
 
 const Catalog: React.FC = () => {
   const products = useAppSelector(productSelectors.selectAll);
-  const { productsLoaded, status } = useAppSelector((state) => state.catalog);
+  const { productsLoaded, status, filtersLoaded } = useAppSelector((state) => state.catalog);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!productsLoaded) dispatch(fetchProductsAsync());
   }, [productsLoaded, dispatch]);
+
+  useEffect(() => {
+    if (!filtersLoaded) dispatch(fetchFiltersAsync());
+  }, [filtersLoaded, dispatch]);
 
   if (status.includes('pending')) return <LoadingComponent message="Loading products..." />;
 
--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -26,11 +26,25 @@ export const fetchProductAsync = createAsyncThunk<Product, number>(
   },
 );
 
+export const fetchFiltersAsync = createAsyncThunk(
+  'catalog/fetchFilters',
+  async (_, thunkAPI) => {
+    try {
+      return await agent.Catalog.fetchFilters();
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue({ error: error.data });
+    }
+  },
+);
+
 export const catalogSlice = createSlice({
   name: 'catalog',
   initialState: productsAdapter.getInitialState({
     productsLoaded: false,
+    filtersLoaded: false,
     status: 'idle',
+    categories: [],
+    types: [],
   }),
   reducers: {},
   extraReducers: ((builder) => {
@@ -56,6 +70,19 @@ export const catalogSlice = createSlice({
     builder.addCase(fetchProductAsync.rejected, (state, action) => {
       console.log(action.payload);
       state.status = 'idle';
+    });
+    builder.addCase(fetchFiltersAsync.pending, (state, action) => {
+      state.status = 'pendingFetchFilters';
+    });
+    builder.addCase(fetchFiltersAsync.fulfilled, (state, action) => {
+      state.categories = action.payload.categories;
+      state.types = action.payload.types;
+      state.filtersLoaded = true;
+      state.status = 'idle';
+    });
+    builder.addCase(fetchFiltersAsync.rejected, (state, action) => {
+      state.status = 'idle';
+      console.log(action.payload);
     });
   }),
 });
