02a3f5670e2a16e3021fe5823c35c91e21904b5e
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-21 16:18:31 +0300

    feat: create login form

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,9 +1,72 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  Avatar, Box, Button, Container, Grid, Link, Paper, TextField, Typography,
+} from '@mui/material';
+import { LockOutlined } from '@mui/icons-material';
 
-const Login = () => {
-  console.log('');
+const Login: React.FC = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
   return (
-    <div>Login</div>
+    <Container
+      component={Paper}
+      maxWidth="sm"
+      sx={{
+        display: 'flex', flexDirection: 'column', alignItems: 'center', p: 4,
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlined />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Sign In
+        </Button>
+        <Grid container>
+          <Grid item>
+            <Link component={RouterLink} to="register">
+              Don&apos;t have an account? Sign Up
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
