efc4f7f3a117d1f8087e067e9f97b7b00539f373
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-21 15:37:56 +0300

    feat: configure swagger to send the auth token

--- API/Program.cs
+++ API/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +15,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(configuration =>
+{
+  var jwtSecurityScheme = new OpenApiSecurityScheme
+  {
+    BearerFormat = "JWT",
+    Name = "Authorization",
+    In = ParameterLocation.Header,
+    Type = SecuritySchemeType.ApiKey,
+    Scheme = JwtBearerDefaults.AuthenticationScheme,
+    Description = "Put Bearer + your token in the box below",
+    Reference = new OpenApiReference
+    {
+      Id = JwtBearerDefaults.AuthenticationScheme,
+      Type = ReferenceType.SecurityScheme
+    }
+  };
+
+  configuration.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+  configuration.AddSecurityRequirement(new OpenApiSecurityRequirement
+  {
+    {
+      jwtSecurityScheme,Array.Empty<string>()
+    }
+  });
+});
 builder.Services.AddDbContext<StoreContext>(options =>
 {
   options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -51,7 +76,10 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
   app.UseSwagger();
-  app.UseSwaggerUI();
+  app.UseSwaggerUI(configuration =>
+  {
+    configuration.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+  });
 }
 
 app.UseCors(options =>
