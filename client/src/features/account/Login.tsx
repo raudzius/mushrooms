f489f497090f6242fba4a8df20917705bb337c56
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-21 19:33:06 +0300

    feat: client side login form validation

--- client/src/features/account/Login.tsx
+++ client/src/features/account/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import {
-  Avatar, Box, Button, Container, Grid, Link, Paper, TextField, Typography,
+  Avatar, Box, Container, Grid, Link, Paper, TextField, Typography,
 } from '@mui/material';
 import { LockOutlined } from '@mui/icons-material';
 import { FieldValues, useForm } from 'react-hook-form';
@@ -9,10 +9,16 @@ import { LoadingButton } from '@mui/lab';
 import agent from '../../app/api/agent';
 
 const Login: React.FC = () => {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm();
+  const { register, handleSubmit, formState: { isSubmitting, errors, isValid } } = useForm({
+    mode: 'onTouched',
+  });
 
   const submitForm = async (data: FieldValues) => {
-    await agent.Account.login(data);
+    try {
+      await agent.Account.login(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -29,27 +35,32 @@ const Login: React.FC = () => {
       <Typography component="h1" variant="h5">
         Sign in
       </Typography>
-      <Box component="form" onSubmit={() => handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
+      <Box component="form" onSubmit={handleSubmit(submitForm)} noValidate sx={{ mt: 1 }}>
         <TextField
           margin="normal"
           fullWidth
           label="Username"
           autoFocus
-          {...register('username')}
+          error={!!errors.username}
+          helperText={errors?.username?.message as string}
+          {...register('username', { required: 'Username is required' })}
         />
         <TextField
           margin="normal"
           fullWidth
           label="Password"
           type="password"
-          {...register('password')}
+          error={!!errors.password}
+          helperText={errors?.password?.message as string}
+          {...register('password', { required: 'Password is required' })}
         />
         <LoadingButton
-          loading={isSubmitting}
           type="submit"
           fullWidth
           variant="contained"
           sx={{ mt: 3, mb: 2 }}
+          loading={isSubmitting}
+          disabled={!isValid}
         >
           Sign In
         </LoadingButton>
