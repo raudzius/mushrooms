24376d43f04d8b0da76abd72499d009b2dcbe083
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-23 06:15:16 +0300

    feat: use react hook form context

--- client/src/features/checkout/CheckoutPage.tsx
+++ client/src/features/checkout/CheckoutPage.tsx
@@ -2,6 +2,7 @@ import {
   Box, Button, Paper, Step, StepLabel, Stepper, Typography,
 } from '@mui/material';
 import React, { useState } from 'react';
+import { FieldValues, FormProvider, useForm } from 'react-hook-form';
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 import Review from './Review';
@@ -22,60 +23,65 @@ const getStepContent = (step: number) => {
 };
 
 const CheckoutPage: React.FC = () => {
+  const methods = useForm();
   const [activeStep, setActiveStep] = useState(0);
 
-  const handleNext = () => {
+  function handleNext(data: FieldValues) {
+    if (activeStep === 0) {
+      console.log(data);
+    }
     setActiveStep(activeStep + 1);
-  };
+  }
 
   const handleBack = () => {
     setActiveStep(activeStep - 1);
   };
 
   return (
-
-    <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-      <Typography component="h1" variant="h4" align="center">
-        Checkout
-      </Typography>
-      <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-        {steps.map((label) => (
-          <Step key={label}>
-            <StepLabel>{label}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <>
-          <Typography variant="h5" gutterBottom>
-            Thank you for your order.
-          </Typography>
-          <Typography variant="subtitle1">
-            Your order number is #2001539. We have emailed your order
-            confirmation, and will send you an update when your order has
-            shipped.
-          </Typography>
-        </>
-      ) : (
-        <>
-          {getStepContent(activeStep)}
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            {activeStep !== 0 && (
-              <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                Back
+    <FormProvider {...methods}>
+      <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+        <Typography component="h1" variant="h4" align="center">
+          Checkout
+        </Typography>
+        <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <>
+            <Typography variant="h5" gutterBottom>
+              Thank you for your order.
+            </Typography>
+            <Typography variant="subtitle1">
+              Your order number is #2001539. We have emailed your order
+              confirmation, and will send you an update when your order has
+              shipped.
+            </Typography>
+          </>
+        ) : (
+          <Box component="form" onSubmit={methods.handleSubmit(handleNext)}>
+            {getStepContent(activeStep)}
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              {activeStep !== 0 && (
+                <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                  Back
+                </Button>
+              )}
+              <Button
+                variant="contained"
+                type="submit"
+                sx={{ mt: 3, ml: 1 }}
+              >
+                {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
               </Button>
-            )}
-            <Button
-              variant="contained"
-              onClick={handleNext}
-              sx={{ mt: 3, ml: 1 }}
-            >
-              {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-            </Button>
+            </Box>
           </Box>
-        </>
-      )}
-    </Paper>
+        )}
+      </Paper>
+    </FormProvider>
   );
 };
 
