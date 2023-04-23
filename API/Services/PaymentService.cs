637c143a938eca556d1f029f0464720d723161cc
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-04-23 18:35:44 +0300

    feat: create payments controller

--- API/Services/PaymentService.cs
+++ API/Services/PaymentService.cs
@@ -27,8 +27,6 @@ namespace API.Services
           PaymentMethodTypes = new List<string> { "card" }
         };
         intent = await service.CreateAsync(options);
-        basket.PaymentIntentId = intent.Id;
-        basket.ClientSecret = intent.ClientSecret;
       }
       else
       {
